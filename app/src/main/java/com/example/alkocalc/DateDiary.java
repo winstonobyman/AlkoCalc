caa5d66c3f32f6924917d3665e161e3cf34deb2b
ivan-z <zaykin.ivan.and@gmail.com>
2020-05-25 03:25:22 +0300

    Small changes and info added.

--- app/src/main/java/com/example/alkocalc/DateDiary.java
+++ app/src/main/java/com/example/alkocalc/DateDiary.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -23,6 +24,13 @@ public class DateDiary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_diary);
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Для удаления зажмите запись", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP, 0, 50);
+        toast.show();
+
+
         recordsList = DiaryUtils.loadRecordsList(this);
         ListView lvDiary = findViewById(R.id.listViewTest);
         final RecordAdapter customAdapter = new RecordAdapter(this, R.layout.itemlistrow, recordsList);
---
+++ app/src/main/java/com/example/alkocalc/InfoPanel.java
@@ -0,0 +1,14 @@
+package com.example.alkocalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_panel);
+    }
+}
